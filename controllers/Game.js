67908b1d3f747111454a4d61eb06e6407d5c68c3
Denys Lygach <denisligach98@gmail.com>
2024-04-25 10:04:19 +0300

    canvas event listeners added

--- controllers/Game.js
+++ controllers/Game.js
@@ -6,7 +6,30 @@ export class Game {
         this.width = canvas.width;
         this.height = canvas.height;
         this.player = new Player(this);
+        this.mouse = {
+            x: this.width * 0.5,
+            y: this.height * 0.5,
+            pressed: false
+        }
+
+        canvas.addEventListener('mousedown', e => {
+            this.mouse.x = e.offsetX;
+            this.mouse.y = e.offsetY;
+            this.mouse.pressed = true;
+        });
+
+        canvas.addEventListener('mouseup', e => {
+            this.mouse.x = e.offsetX;
+            this.mouse.y = e.offsetY;
+            this.mouse.pressed = false;
+        });
+
+        canvas.addEventListener('mousemove', e => {
+            this.mouse.x = e.offsetX;
+            this.mouse.y = e.offsetY;
+        });
     }
+
     render(context) {
         this.player.draw(context);
     }
